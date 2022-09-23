229cadbfaef3c0e8787e9b8c5f2400119f22789d
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-09-23 17:17:21 -0300

    implementação escolha de iniciais e cidade

---
+++ initCity.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./css/iniciais.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+    <div id="espaco">
+
+        <a href="city.php?id=charmander">
+            
+        <img src="./videos/iniciais/char.png" width="350" height="300">
+        <p id="charmander"><img id="icon" src="./videos/iniciais/icons/fire.png">Charmander</p>
+        </a>        
+
+    </div>
+
+    <div id="espaco">
+
+        <a href="city.php?id=squirtle">
+        <img src="./videos/iniciais/squir.png" width="350" height="300">
+        <p  id="squirtle"><img id="icon" src="./videos/iniciais/icons/water.png">Charmander</p>
+
+   
+        </a>
+    </div>
+
+    <div id="espaco">
+    <a href="city.php?id=bulbasaur">
+        <img src="./videos/iniciais/bulba.png" width="350" height="300">
+         <p id="bulba"><img id="icon" src="./videos/iniciais/icons/leaf.png">Charmander</p>
+
+     
+        </a>
+    </div>
+
+
+
+
+
+
+
+</body>
+
+</html>
