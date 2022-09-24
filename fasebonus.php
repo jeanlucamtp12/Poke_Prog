fafd500a228a1d2ab9b4a144bdc58a830cb962e2
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-09-23 21:27:29 -0300

    fase bonus, começo da implementação

---
+++ fasebonus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/fasebonus.css">
+
+    <title>Document</title>
+</head>
+<body>
+     
+    <div class="jogobonus">
+
+    <img id="pedra" src="./img/bonus/float_stone.png">
+    <img id="inicial" src="./img/bonus/bulbasaur.png">
+
+    <img id="zubat" src="./img/bonus/zubat.gif">
+    <img id="golbat" src="./img/bonus/golbat.gif">
+    <img id="nuvens" src="./img/bonus/clouds.png">
+    <img id="venomoth" src="./img/bonus/venomoth.gif">
+    <img id="grama" src="./img/bonus/grama.png">
+
+
+
+
+
+
+    </div>
+
+</body>
+</html>
