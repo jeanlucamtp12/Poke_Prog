8fa928088dca1e35b64ea2721dc1ae07ce5ae665
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-09-28 00:19:37 -0300

    corrigindo responsividade

--- caixas.php
+++ caixas.php
@@ -54,7 +54,7 @@
 
     const mapearUnowm = (unowm) => {
 
-        const cont = document.querySelectorAll("#aU, #bU, #cU, #áU, #eU, #fU, #gU, #ãU ,#iU,#éU, #íU , #lU, #mU , #nU , #oU , #pU, #qU, #rU, #sU,  #tU, #uU, #vU, #wU, #xU, #yU, #zU, #çU, #úU");
+        const cont = document.querySelectorAll("#aU, #bU, #cU, #áU, #eU, #fU, #gU, #ãU ,#iU,#éU, #íU , #lU, #mU , #nU , #oU , #pU, #qU, #rU, #sU,  #tU, #uU, #vU, #wU, #xU, #yU, #zU, #àU, #úU");
 
         for (var i = 0; i < cont.length; i++) {
 
@@ -79,6 +79,8 @@
                 var caminho_imagem = './img/unowns/' + remove_string + '.gif';
 
                 document.getElementById(remove_string).innerHTML = "<img src=" + caminho_imagem + ">";
+
+                console.console(caminho_imagem);
 
 
 
@@ -113,153 +115,170 @@
 
     <div class="tamanhoTela">
 
+        <div class="titulo">
+            <h1>Informe o tipo de variavel dentro da caixa</h1>
+        </div>
 
-        <h1>Informe o tipo de variavel dentro da caixa</h1>
-
-
-        <section class="caixa">
-            <div class="texto">
-                <h2 id="alterar">Numero = 12</h2>
-            </div>
-            <img src="./img/fases unowms/caixa.png">
-        </section>
-
-
-
-
-        <div class="teclado">
-            <h1>Teclado Unowm</h1>
-            <div class="caixaTeclado">
-                <h6>A</h6>
-                <img class="circles" id="aU" src="./img/unowns/a.gif" onclick="mapearUnowm('aU')" >
-            </div>
-            <div class="caixaTeclado">
-                <h6>B</h6>
-                <img class="circles" id="bU" src="./img/unowns/b.gif" onclick="mapearUnowm('bU')" >
-            </div>
-            <div class="caixaTeclado">
-                <h6>C</h6>
-                <img class="circles" id="cU" src="./img/unowns/c.gif" onclick="mapearUnowm('cU')">
-            </div>
-            <div class="caixaTeclado">
-                <h6>D</h6>
-                <img class="circles" id="áU" src="./img/unowns/á.gif" onclick="mapearUnowm('áU')" >
-            </div>
-            <div class="caixaTeclado">
-                <h6>E</h6>
-                <img class="circles" id="eU" src="./img/unowns/e.gif" onclick="mapearUnowm('eU')" >
-            </div>
-            <div class="caixaTeclado">
-                <h6>F</h6>
-                <img class="circles" id="fU" src="./img/unowns/f.gif" onclick="mapearUnowm('fU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>G</h6>
-                <img class="circles" id="gU" src="./img/unowns/g.gif" onclick="mapearUnowm('gU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>H</h6>
-                <img class="circles" id="ãU" src="./img/unowns/ã.gif" onclick="mapearUnowm('ãU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>I</h6>
-                <img class="circles" id="iU" src="./img/unowns/i.gif" onclick="mapearUnowm('iU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>J</h6>
-                <img class="circles" id="éU" src="./img/unowns/é.gif" onclick="mapearUnowm('éU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>K</h6>
-                <img class="circles" id="íU" src="./img/unowns/í.gif" onclick="mapearUnowm('íU')" ;>
-            </div>
-
-            <div class="caixaTeclado">
-                <h6>L</h6>
-                <img class="circles" id="lU" src="./img/unowns/l.gif" onclick="mapearUnowm('lU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>M</h6>
-                <img class="circles" id="mU" src="./img/unowns/m.gif" onclick="mapearUnowm('mU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>N</h6>
-                <img class="circles" id="nU" src="./img/unowns/n.gif" onclick="mapearUnowm('nU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>O</h6>
-                <img class="circles" id="oU" src="./img/unowns/o.gif" onclick="mapearUnowm('oU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>P</h6>
-                <img class="circles" id="pU" src="./img/unowns/p.gif" onclick="mapearUnowm('pU')" ;>
-            </div>
-
-            <div class="caixaTeclado">
-                <h6>Q</h6>
-                <img class="circles" id="qU" src="./img/unowns/q.gif" onclick="mapearUnowm('qU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>R</h6>
-                <img class="circles" id="rU" src="./img/unowns/r.gif" onclick="mapearUnowm('rU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>S</h6>
-                <img class="circles" id="sU" src="./img/unowns/s.gif" onclick="mapearUnowm('sU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>T</h6>
-                <img class="circles" id="tU" src="./img/unowns/t.gif" onclick="mapearUnowm('tU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>U</h6>
-                <img class="circles" id="uU" src="./img/unowns/u.gif" onclick="mapearUnowm('uU')" ;>
-            </div>
-
-            <div class="caixaTeclado">
-                <h6>V</h6>
-                <img class="circles" id="vU" src="./img/unowns/v.gif" onclick="mapearUnowm('vU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>W</h6>
-                <img class="circles" id="wU" src="./img/unowns/w.gif" onclick="mapearUnowm('wU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>X</h6>
-                <img class="circles" id="xU" src="./img/unowns/x.gif" onclick="mapearUnowm('xU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>Y</h6>
-                <img class="circles" id="yU" src="./img/unowns/y.gif" onclick="mapearUnowm('yU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>Z</h6>
-                <img class="circles" id="zU" src="./img/unowns/z.gif" onclick="mapearUnowm('zU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>?</h6>
-                <img class="circles" id="çU" src="./img/unowns/ó.gif" onclick="mapearUnowm('çU')" ;>
-            </div>
-            <div class="caixaTeclado">
-                <h6>!</h6>
-                <img class="circles" id="úU" src="./img/unowns/ú.gif" onclick="mapearUnowm('úU')" ;>
-            </div>
+        <div class="caixaConteudo">
+            <h2 id="alterar">Numero = 12</h2>
+            <img src="./img/fases unowms/caixa.png" width="130px">
         </div>
 
 
-        <div class="caixaResposta">
-            <span id="a"></span> <span id="b"></span> <span id="c"> </span> <span id="á"></span> <span id="e"></span><span id="f"></span><span id="g"></span> <span id="ã"></span> <span id="i"></span> <span id="é"></span> <span id="í"></span> <span id="l"></span> <span id="m"></span> <span id="n"></span> <span id="o"></span><span id="p"></span><span id="q"></span> <span id="r"></span> <span id="s"></span> <span id="t"></span><span id="u"></span><span id="v"></span> <span id="w"></span> <span id="x"></span> <span id="y"></span><span id="z"></span><span id="çU"></span> <span id="úU"></span>
+        <div class="caixas">
+
+            <div class="filhoTela">
+                <div class="teclado">
+                    <h1>Teclado Unowm</h1>
+                    <div class="caixaTeclado">
+                        <h6>A</h6>
+                        <img class="circles" id="aU" src="./img/unowns/a.gif" onclick="mapearUnowm('aU')">
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>B</h6>
+                        <img class="circles" id="bU" src="./img/unowns/b.gif" onclick="mapearUnowm('bU')">
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>C</h6>
+                        <img class="circles" id="cU" src="./img/unowns/c.gif" onclick="mapearUnowm('cU')">
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>D</h6>
+                        <img class="circles" id="áU" src="./img/unowns/á.gif" onclick="mapearUnowm('áU')">
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>E</h6>
+                        <img class="circles" id="eU" src="./img/unowns/e.gif" onclick="mapearUnowm('eU')">
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>F</h6>
+                        <img class="circles" id="fU" src="./img/unowns/f.gif" onclick="mapearUnowm('fU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>G</h6>
+                        <img class="circles" id="gU" src="./img/unowns/g.gif" onclick="mapearUnowm('gU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>H</h6>
+                        <img class="circles" id="ãU" src="./img/unowns/ã.gif" onclick="mapearUnowm('ãU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>I</h6>
+                        <img class="circles" id="iU" src="./img/unowns/i.gif" onclick="mapearUnowm('iU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>J</h6>
+                        <img class="circles" id="éU" src="./img/unowns/é.gif" onclick="mapearUnowm('éU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>K</h6>
+                        <img class="circles" id="íU" src="./img/unowns/í.gif" onclick="mapearUnowm('íU')" ;>
+                    </div>
+
+                    <div class="caixaTeclado">
+                        <h6>L</h6>
+                        <img class="circles" id="lU" src="./img/unowns/l.gif" onclick="mapearUnowm('lU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>M</h6>
+                        <img class="circles" id="mU" src="./img/unowns/m.gif" onclick="mapearUnowm('mU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>N</h6>
+                        <img class="circles" id="nU" src="./img/unowns/n.gif" onclick="mapearUnowm('nU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>O</h6>
+                        <img class="circles" id="oU" src="./img/unowns/o.gif" onclick="mapearUnowm('oU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>P</h6>
+                        <img class="circles" id="pU" src="./img/unowns/p.gif" onclick="mapearUnowm('pU')" ;>
+                    </div>
+
+                    <div class="caixaTeclado">
+                        <h6>Q</h6>
+                        <img class="circles" id="qU" src="./img/unowns/q.gif" onclick="mapearUnowm('qU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>R</h6>
+                        <img class="circles" id="rU" src="./img/unowns/r.gif" onclick="mapearUnowm('rU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>S</h6>
+                        <img class="circles" id="sU" src="./img/unowns/s.gif" onclick="mapearUnowm('sU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>T</h6>
+                        <img class="circles" id="tU" src="./img/unowns/t.gif" onclick="mapearUnowm('tU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>U</h6>
+                        <img class="circles" id="uU" src="./img/unowns/u.gif" onclick="mapearUnowm('uU')" ;>
+                    </div>
+
+                    <div class="caixaTeclado">
+                        <h6>V</h6>
+                        <img class="circles" id="vU" src="./img/unowns/v.gif" onclick="mapearUnowm('vU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>W</h6>
+                        <img class="circles" id="wU" src="./img/unowns/w.gif" onclick="mapearUnowm('wU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>X</h6>
+                        <img class="circles" id="xU" src="./img/unowns/x.gif" onclick="mapearUnowm('xU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>Y</h6>
+                        <img class="circles" id="yU" src="./img/unowns/y.gif" onclick="mapearUnowm('yU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>Z</h6>
+                        <img class="circles" id="zU" src="./img/unowns/z.gif" onclick="mapearUnowm('zU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>?</h6>
+                        <img class="circles" id="àU" src="./img/unowns/à.gif" onclick="mapearUnowm('àU')" ;>
+                    </div>
+                    <div class="caixaTeclado">
+                        <h6>!</h6>
+                        <img class="circles" id="úU" src="./img/unowns/ú.gif" onclick="mapearUnowm('úU')" ;>
+
+                    </div>
+                </div>
+            </div>
+            <div class="filhoTela">
+                <div class="inicial">
+                    <h2>Ajuda</h2>
+                    <img src="./img/bonus/1.png" width="190px" alt="Ajuda" title="Ajuda" onclick="exibeAjuda()" style="cursor:pointer">
+                    <h2 id="ajuda" style="display:none">Int = Números Inteiros <br>Float = Números Com Virgula <br>String = Palavras E Letras<br>Booleano = Verdadeiro Ou Falso </h2>
+
+                </div>
+
+
+            </div>
+
+            <div class="filhoTela">
+                <div class="caixaResposta">
+                    <span id="a"></span> <span id="b"></span> <span id="c"> </span> <span id="á"></span> <span id="e"></span><span id="f"></span><span id="g"></span> <span id="ã"></span> <span id="i"></span> <span id="é"></span> <span id="í"></span> <span id="l"></span> <span id="m"></span> <span id="n"></span> <span id="o"></span><span id="p"></span><span id="q"></span> <span id="r"></span> <span id="s"></span> <span id="t"></span><span id="u"></span><span id="v"></span> <span id="w"></span> <span id="x"></span> <span id="y"></span><span id="z"></span><span id="àU"></span> <span id="úU"></span>
+                    <button id="botaoResposta" onclick="verificaTipo()">Enviar</button>
+
+                </div>
+            </div>
+
+
+
         </div>
-        <button id="botaoResposta" onclick="verificaTipo()">Enviar</button>
+    </div>
 
 
 
 
 
-        <div class="blocoInicial">
-            <img class="inicial" src="./img/bonus/1.png" alt="Ajuda" title="Ajuda" onclick="exibeAjuda()" style="cursor:pointer">
-            <h2 id="ajuda" style="display:none">Se vira gay</h3>
-        </div>
+    </div>
+
+
 
 
     </div>
