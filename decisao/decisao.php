3b8b12d08e67b85bdd848da905d7f696988e7fb3
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-10-04 23:29:14 -0300

    começo implementacao fase excecoes

--- decisao/decisao.php
+++ decisao/decisao.php
@@ -33,6 +33,8 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
 
         <img class="nuvens" src="../img/decisao/clouds.png">
+   
+
 
 
 
---
+++ excecoes/excecoes.php
@@ -0,0 +1,129 @@
+<?php
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../css/excecoes.css">
+
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="cidade">
+
+
+
+
+
+        <div>
+            <img class="nuvens" src="../img/excecoes/nuvens.png"></img>
+
+        </div>
+
+        <div>
+            <img class="arvore" src="../img/excecoes/arvore.png"></img>
+
+        </div>
+        
+        <div>
+            <img class="arvore2" src="../img/excecoes/arvore2.png" ></img>
+
+        </div>
+        
+        <div>
+            <img class="mato um" src="../img/excecoes/mato.png"></img>
+
+        </div>
+        <div>
+            <img class="mato dois" src="../img/excecoes/mato2.png"></img>
+
+        </div>
+
+        <div class="teia">
+            <img class="spinarak" src="../img/excecoes/spinarak.png"></img>
+
+        </div>
+
+        <div class="mastro">
+            <img class="cacturne" src="../img/excecoes/cacturne.png"></img>
+
+        </div>
+
+        <div class="zubats">
+            <img class="zubat primeiro" src="../img/excecoes/zubat.png"></img>
+            <img class="zubat segundo" src="../img/excecoes/zubat.png" ></img>
+            <img class="zubat terceiro" src="../img/excecoes/zubat.png" ></img>
+            <img class="zubat quarto" src="../img/excecoes/zubat.png" ></img>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div>
+            <img class="grama" src="../img/excecoes/grama.png"></img>
+            <img class="grama" src="../img/excecoes/grama.png" style="left:0px;"></img>
+
+        </div>
+
+
+
+        <div>
+            <img class="estrela " src="../img/excecoes/estrela.png"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 1150px; top: 150px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 950px; top: 250px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 850px; top: 200px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 750px; top: 350px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 650px; top: 150px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 1550px; top: 200px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 1250px; top: 450px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 1150px; top: 150px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 950px; top: 390px;"></img>
+            <img class="estrela " src="../img/excecoes/estrela.png" style="left: 850px; top: 200px;"></img>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+</body>
+
+</html>
