11a419e987b81161bc7f30cd37cf195acbb25ec4
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2023-05-30 00:25:26 -0300

    alteraçõs exceções e demais fases

--- electron/electron-win32-x64/resources/app/Quizes/questions.js
+++ electron/electron-win32-x64/resources/app/Quizes/questions.js
@@ -2,17 +2,17 @@ var perguntas = [
     {
       question: "Qual dessas não é um tipo de variável?",
       answers: [
+        { option: "Letras", correct: false },
+        { option: "Pergunta", correct: true },
         { option: "Números inteiros", correct: false },
         { option: "Palavras", correct: false },
-        { option: "Números reais", correct: false },
-        { option: "Contas", correct: true },
       ],
     },
     {
-      question: "Qual tipo de caixinha (variável) armazena 123?",
+      question: "Qual tipo de caixinha (variável) armazena 'Olá Mundo'?",
       answers: [
-        { option: "Numeros inteiros", correct: true },
-        { option: "Palavras", correct: false },
+        { option: "Numeros inteiros", correct: false },
+        { option: "Palavras", correct: true },
         { option: "Booleanos", correct: false },
         { option: "Letras", correct: false },
       ],
@@ -25,27 +25,21 @@ var perguntas = [
         { option: "Colocar um valor dentro da caixa", correct: true },
       ],
     },
+
+
     {
-      question: "O que significa 'se uma condição for verdadeira / acerta a pergunta durante a batalha' em uma estrutura de decisão?",
+      question: "O que melhor descreve o objetivo apresentado na fase de decisão?",
       answers: [
-        { option: "Ataca o oponente", correct: true },
-        { option: "Nada acontece", correct: false },
-        { option: "É atacado pelo oponente", correct: false },
+        { option: "Tomar uma decisão a partir de uma questão apresentada para o usuário,<br> onde a resposta pode influenciar na barra de vida que o usuário terá durante o jogo", correct: false },
+        { option: "Tomar uma decisão a partir de uma questão apresentada para o usuário,<br> onde a resposta pode acarretar em um ataque ao inimigo ou em um ataque direcionado pelo inimigo", correct: true },
       ],
     },
 
-    {
-      question: "O que significa 'se uma condição for falsa / errar a pergunta durante a batalha' em uma estrutura de decisão?",
-      answers: [
-        { option: "Ataca o oponente", correct: false },
-        { option: "É atacado pelo oponente", correct: true },
-        { option: "Nada acontece", correct: false },
-      ],
-    },
+
+
 
     {
-
-      question: "  O que significa o operador == durante as perguntas da fase ?",
+      question: "  O que significa o operador == durante as perguntas da fase de decisões?",
       answers: [
         { option: "Multiplicação", correct: false },
         { option: "Divisão", correct: false },
@@ -59,27 +53,13 @@ var perguntas = [
 
 
     {
-      question: "Qual ação você teve que executar de forma repetida várias vezes durante a fase?",
+      question: "Além de carregar os troncos, qual outras ações você teve que realizar repetidas vezes durante a fase?",
       answers: [
-        { option: "Carregar os troncos para projetar a ponte", correct: true },
-        { option: "Atravessar a ponte", correct: false },
-        { option: "Usar poderes", correct: false },
+        { option: "Correr, lançar projéteis, voar", correct: false },
+        { option: "Pular, evitar ataques e correr", correct: true },
+        { option: "Coletar peixes, procurar tesouros", correct: false },
       ],
     },
-
-
-
-
-    {
-      question: "Realizar uma mesma tarefa diversas vezes, faz com que você crie?",
-      answers: [
-        { option: "Um caminho", correct: false },
-        { option: "Um triangulo", correct: false },
-        { option: "Um loop", correct: true },
-      ],
-    },
-
-
 
 
     {
@@ -95,18 +75,18 @@ var perguntas = [
 
 
     { 
-      question: "Qual era o tamanho do vetor de Falinks que você teve que montar?",
+      question: "Caso encontremos um Falinks com 30 partes perdidas, qual tamanho de caixa de vetor seria necessário para guardá-los? ",
       answers: [
-        { option: "16", correct: false },
-        { option: "10", correct: true },
-        { option: "22", correct: false },
-        { option: "12", correct: false },
+        { option: "6", correct: false },
+        { option: "20", correct: false },
+        { option: "30", correct: true },
+        { option: "52", correct: false },
       ],
     },
 
 
     {
-      question: "Você acha que seria possível obter um vetor de Falinks maior que 10?",
+      question: "Você acha que seria possível obter um vetor de Falinks maior que 30, caso fosse necessário?",
       answers: [
         { option: "sim", correct: true },
         { option: "não", correct: false },
@@ -114,32 +94,15 @@ var perguntas = [
     },
 
 
-    {
-      question: "O que descreve melhor o que lhe foi pedido durante a segunda fase dos Falinks?",
-      answers: [
-        { option: "Atribuição de ids para cada Falinks do vetor", correct: false },
-        { option: "Atribuição de nomes para alguns Falinks do vetor", correct: true },
-        { option: "Atribuição de número para cada Falinks do vetor", correct: false },
-      ],
-    },
 
-
-    {
-      question: "Caso não utilizasse as funções dos dispositivos Rotom, como ficaria a casa?",
-      answers: [
-        { option: "Normal, não alteraria nada", correct: false },
-        { option: "Bagunçada, a casa continuaria desarrumada", correct: true },
-        { option: "Limpa, não alteraria o estado da casa", correct: false },
-      ],
-    },
 
     {
       question: "Qual era o maior objetivo da fase de funções?",
       answers: [
-        { option: "Coletar as frutas", correct: false },
-        { option: "Lavar roupas", correct: false },
-        { option: "Organizar a casa", correct: true },
-        { option: "Fazer um bolo", correct: false },
+        { option: "Coletar itens, cozinhar e lavar", correct: false },
+        { option: "Coletar itens, separar itens, utilizar itens ", correct: false },
+        { option: "Coletar alimentos, coletar roupas, categorizar itens, preparar alimentos. ", correct: true },
+        { option: "Fazer uma pizza", correct: false },
       ],
     },
 
@@ -153,17 +116,10 @@ var perguntas = [
       ],
     },
 
-    {
-      question: "O que causa uma exceção/diferenciação na fase?",
-      answers: [
-        { option: "Pokemon diferente dos convencionais", correct: true },
-        { option: "Pokemon iguais as convencionais", correct: false },
-        { option: "Fantasmas no cemitério", correct: false },
-      ],
-    },
+
 
     {
-      question: "Caso os Pokémon shinies não fossem capturados, o que iria acontecer?",
+      question: "Caso os Pokémon shinies não fossem capturados, o que iria acontecer durante a fase no cemitério?",
       answers: [
         { option: "Desordem, os Pokémon shiny continuariam afetando a convivência dos demais ", correct: true },
         { option: "Nada ocorreria, são amigos", correct: false },
@@ -185,6 +141,18 @@ var perguntas = [
       answers: [
         { option: "Pegou o maior bloco da pilha e moveu para o terceiro pino.<br> Depois, usou a fórmula novamente para mover todos os blocos menores que ficaram no primeiro pino para o segundo pino.<br> E por último, usou a fórmula mais uma vez para mover todos os discos menores do segundo pino para o terceiro pino.", correct: true },
         { option: "Pegou o menor bloco da pilha e moveu para o terceiro pino.<br> Depois, usou a fórmula novamente para mover todos os blocos maiores que ficaram no primeiro pino para o segundo pino.<br> E por último, usou a fórmula mais uma vez para mover todos os discos maiores do segundo pino para o terceiro pino.", correct: false },
+      ],
+    
+    },
+
+    {
+      question: "Qual é o menor número de movimentos necessários para resolver a Torre de Hanói com 5 pilhas de magnemite?",
+      answers: [
+        { option: "31 movimentos", correct: true },
+        { option: "21 movimentos", correct: false },
+        { option: "1 movimentos", correct: false },
+        { option: "11 movimentos", correct: false },
+
       ],
     
     },
