cfd70614ed39916a9c88030b9866b9c979524512
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2023-06-08 16:06:13 -0300

    novas alterações realizadas

--- electron/electron-win32-x64/resources/app/Quizes/question2.js
+++ electron/electron-win32-x64/resources/app/Quizes/question2.js
@@ -18,6 +18,18 @@ var perguntas = [
       ],
     },
    
+
+    {
+      question: "Como você atribui um valor a uma caixa variável?",
+      answers: [
+        { option: "Desenhando sobre a caixa", correct: false },
+        { option: "Pedindo para a variável escolher seu valor", correct: false },
+        { option: "Usando o sinal de igual (=)", correct: true },
+        { option: "Nenhuma das alternativas", correct: false },
+      ],
+    },
+
+
     {
       question: "O que significa 'se uma condição for verdadeira / acerta a pergunta durante a batalha' em uma estrutura de decisão?",
       answers: [
@@ -36,6 +48,14 @@ var perguntas = [
       ],
     },
 
+    {
+      question: "Existe alguma maneira da condição não ser falsa/errada ou verdadeira/certa durante a batalha?",
+      answers: [
+        { option: "sim", correct: false },
+        { option: "não", correct: true },
+      ],
+    },
+
 
 
     {
@@ -48,14 +68,23 @@ var perguntas = [
     },
 
 
-
-
     {
       question: "Realizar uma mesma tarefa diversas vezes, faz com que você crie?",
       answers: [
         { option: "Um caminho", correct: false },
         { option: "Um triangulo", correct: false },
         { option: "Um loop", correct: true },
+      ],
+    },
+
+
+    {
+      question: "Qual era a condição de parada/vitória da fase?",
+      answers: [
+        { option: "Pegar vidas", correct: false },
+        { option: "Colocar o último tronco na ponte", correct: true },
+        { option: "Desviar de todos os ataques", correct: false },
+        { option: "Andar pela fase", correct: true },
       ],
     },
 
@@ -72,6 +101,16 @@ var perguntas = [
     },
 
 
+    { 
+      question: "O que aconteceria se utilizassem um vetor de 9 posições para armazenar as 10 partes do Falinks?",
+      answers: [
+        { option: "Nada, a ultima cabeça seria colocada no vetor da mesma forma", correct: false },
+        { option: "Erro, uma parte do Falinks não caberia no vetor", correct: true },
+        { option: "O vetor iria quebrar e explusar todas as partes do Falinks armazenadas", correct: false },
+      ],
+    },
+
+
     {
       question: "O que descreve melhor o que lhe foi pedido durante a segunda fase dos Falinks?",
       answers: [
@@ -80,6 +119,7 @@ var perguntas = [
         { option: "Atribuição de número para cada Falinks do vetor", correct: false },
       ],
     },
+
 
 
     {
@@ -98,6 +138,17 @@ var perguntas = [
         { option: "Lavar roupas", correct: false },
         { option: "Organizar a casa", correct: true },
         { option: "Fazer um bolo", correct: false },
+      ],
+    },
+
+
+
+    {
+      question: "Qual desses dispositivos poderia ser colocado dentro da casa das funções para auxiliar os moradores?",
+      answers: [
+        { option: "Guarda roupas, para separar as roupas de acordo com seu tipo", correct: true },
+        { option: "Aspirador de pó, para limpar o chão da casa", correct: false },
+        { option: "Câmera de segurança, para monitorar a casa", correct: false },
       ],
     },
 
@@ -123,7 +174,16 @@ var perguntas = [
       },
 
 
+      {
+        question: "Por que as exceções são importantes em programação?",
+        answers: [
+          { option: "Elas tornam as coisas mais desafiadoras", correct: false },
+          { option: "Elas fornecem dicas e sugestões as pessoas", correct: false },
+          { option: "Elas ajudam a detectar erros e problemas durante a execução de um programa", correct: true },
+        ],
+      },
 
+      
       {
         question: "Quantas hastes são utilizadas na Torre de Hanói?",
         answers: [
@@ -140,6 +200,16 @@ var perguntas = [
           { option: "Força física", correct: false },
           { option: "Lógica e estratégia", correct: true },
           { option: "Habilidades musicais", correct: false },
+        ],
+      
+      },
+
+      {
+        question: "Na sua visão, o que é recursividade em programação?",
+        answers: [
+          { option: "Um tipo de jogo", correct: false },
+          { option: "Um meio de ensino que pode auxiliar professores", correct: false },
+          { option: " Um conceito que permite dividir um problema em subproblemas semelhantes", correct: true },
         ],
       
       },
