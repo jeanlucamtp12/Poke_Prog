33adca49e6dfc0beed7bb3c3ba89a6f226d83324
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2023-06-10 19:06:29 -0300

    decisao/vars  modo dinamico para perguntas

---
+++ electron/variaveis/perguntas.js
@@ -0,0 +1,64 @@
+var perguntas = [
+
+    {
+      question: "idade = 18",
+      answers: [
+        { option: "inteiro"},
+      ],
+    },
+
+
+    {
+        question: "Numero = 222",
+        answers: [
+          { option: "inteiro"},
+        ],
+      
+    },
+
+
+
+    {
+        question: "Nome = 'Ana'",
+        answers: [
+          { option: "palavra"},
+        ],
+    },
+
+
+
+    {
+        question: "Valor = 2,22",
+        answers: [
+          { option: "decimal"},
+        ],
+    },
+
+
+    {
+        question: "Letra = 'A'",
+        answers: [
+          { option: "letra"},
+        ],
+    },
+
+
+    {
+        question: "Dado = Falso",
+        answers: [
+          { option: "booleano"},
+        ],
+    },
+
+
+    {
+        question: "Frase = 'Olá Mundo'",
+        answers: [
+          { option: "palavra"},
+        ],
+    },
+
+
+];
+
+
