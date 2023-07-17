18bb63c7b1b1a5e0e0601aef03fd31729c766b54
jeanlucamtp12 <51718707+jeanlucamtp12@users.noreply.github.com>
2023-07-17 19:00:20 -0300

    Add files via upload

---
+++ electron/variaveis/perguntas.js
@@ -0,0 +1,65 @@
+
+var perguntas = [
+
+    {
+      question: "Numero = 25",
+      answers: [
+        { option: "inteiro"},
+      ],
+    },
+
+
+    {
+        question: "Number = 'Dois'",
+        answers: [
+          { option: "palavra"},
+        ],
+      
+    },
+
+
+
+    {
+        question: "Primeiro = 'A'",
+        answers: [
+          { option: "letra"},
+        ],
+    },
+
+
+
+    {
+        question: "Valor = 2,22",
+        answers: [
+          { option: "decimal"},
+        ],
+    },
+
+
+    {
+        question: "Dia = 12",
+        answers: [
+          { option: "inteiro"},
+        ],
+    },
+
+
+    {
+        question: "Dado = Falso",
+        answers: [
+          { option: "booleano"},
+        ],
+    },
+
+
+    {
+        question: "Frase = 'Olá Mundo'",
+        answers: [
+          { option: "palavra"},
+        ],
+    },
+
+
+];
+
+
