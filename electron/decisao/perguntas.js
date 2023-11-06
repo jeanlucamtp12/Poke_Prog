fec09325293d956f7bdfb13f2793fa694eca7c90
jeanlucamtp12 <51718707+jeanlucamtp12@users.noreply.github.com>
2023-11-06 14:07:01 -0300

    Add files via upload

--- electron/decisao/perguntas.js
+++ electron/decisao/perguntas.js
@@ -1,220 +1,220 @@
 var perguntas = [
 
-    {
-      question: "1 > 2?",      
+  {
+    question: "1 > 2?",      
+    answers: [
+      { option: "sim", correct: false },
+      { option: "não", correct: true },
+    
+    ],
+  },
+
+
+  {
+    question: "Se 26, 24, 20, 18, 14,... Qual é o próximo?",      
+      answers: [
+        { option: "12", correct: true },
+        { option: "10", correct: false },
+      
+      ],
+  },
+ 
+
+
+  {
+    question: "O valor de pi é aproximadamente 3,132...",      
       answers: [
         { option: "sim", correct: false },
         { option: "não", correct: true },
       
       ],
-    },
+  },
 
 
-    {
-      question: "Se 26, 24, 20, 18, 14,... Qual é o próximo?",      
-        answers: [
-          { option: "12", correct: true },
-          { option: "10", correct: false },
-        
-        ],
-    },
-   
-
-
-    {
-      question: "O valor de pi é aproximadamente 3,132...",      
-        answers: [
-          { option: "sim", correct: false },
-          { option: "não", correct: true },
-        
-        ],
-    },
-
-
-    {
-      question: "Se 20 == 25?",      
-        answers: [
-          { option: "não", correct: true },
-          { option: "sim", correct: false },
-        
-        ],
-    },
+  {
+    question: "Se 20 == 25?",      
+      answers: [
+        { option: "não", correct: true },
+        { option: "sim", correct: false },
+      
+      ],
+  },
 
 
 
 
-    {
-      question: "Leandro tem 40 balas chupou 12 e deu 10 para sua irmã. Quantas balas ele tem?",      
-        answers: [
-          { option: "20", correct: false },
-          { option: "18", correct: true },
-        
-        ],
-    },
+  {
+    question: "Leandro tem 40 balas chupou 12 e deu 10 para sua irmã. Quantas balas ele tem?",      
+      answers: [
+        { option: "20", correct: false },
+        { option: "18", correct: true },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quantos números '1's eu tenho de 1 até 191?",      
-        answers: [
-          { option: "132", correct: true },
-          { option: "134", correct: false },
-        
-        ],
-    },
+  {
+    question: "Quantos números '1's eu tenho de 1 até 191?",      
+      answers: [
+        { option: "132", correct: true },
+        { option: "134", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quanto é o dobro 1500?",      
-        answers: [
-          { option: "3000", correct: true },
-          { option: "3500", correct: false },
-        
-        ],
-    },
+  {
+    question: "Quanto é o dobro 1500?",      
+      answers: [
+        { option: "3000", correct: true },
+        { option: "3500", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Se DELTA= b^2-4.a.c, essa informação é verdadeira ou falsa?",      
-        answers: [
-          { option: "falsa", correct: false },
-          { option: "verdadeira", correct: true },
-        
-        ],
-    },
+  {
+    question: "Se DELTA= b^2-4.a.c, essa informação é verdadeira ou falsa?",      
+      answers: [
+        { option: "falsa", correct: false },
+        { option: "verdadeira", correct: true },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quantos planetas existem no sistema solar?",      
-        answers: [
-          { option: "8", correct: true },
-          { option: "7", correct: false },
-        
-        ],
-    },
+  {
+    question: "Quantos planetas existem no sistema solar?",      
+      answers: [
+        { option: "8", correct: true },
+        { option: "7", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quantos meses têm 28 dias?",      
-        answers: [
-          { option: "2", correct: false },
-          { option: "12", correct: true },
-        
-        ],
-    },
+  {
+    question: "Quantos meses têm 28 dias?",      
+      answers: [
+        { option: "2", correct: false },
+        { option: "12", correct: true },
+      
+      ],
+  },
 
 
 
-    {
-      question: "A Lua é maior que a Terra?",      
-        answers: [
-          { option: "sim", correct: false },
-          { option: "não", correct: true },
-        
-        ],
-    },
+  {
+    question: "A Lua é maior que a Terra?",      
+      answers: [
+        { option: "sim", correct: false },
+        { option: "não", correct: true },
+      
+      ],
+  },
 
 
 
-    {
-      question: "12 dividido por 3 é igual a 4?",      
-        answers: [
-          { option: "sim", correct: true },
-          { option: "não", correct: false },
-        
-        ],
-    },
+  {
+    question: "12 dividido por 3 é igual a 4?",      
+      answers: [
+        { option: "sim", correct: true },
+        { option: "não", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "6 x 4 é igual a 24?",      
-        answers: [
-          { option: "não", correct: false },
-          { option: "sim", correct: true },
-        
-        ],
-    },
+  {
+    question: "6 x 4 é igual a 24?",      
+      answers: [
+        { option: "não", correct: false },
+        { option: "sim", correct: true },
+      
+      ],
+  },
 
 
-    {
-      question: "O Sol é uma estrela?",      
-        answers: [
-          { option: "sim", correct: true },
-          { option: "não", correct: false },
-        
-        ],
-    },
+  {
+    question: "O Sol é uma estrela?",      
+      answers: [
+        { option: "sim", correct: true },
+        { option: "não", correct: false },
+      
+      ],
+  },
 
 
-    {
-      question: "A pizza surgiu na França?",      
-        answers: [
-          { option: "não", correct: true },
-          { option: "sim", correct: false },
-        
-        ],
-    },
+  {
+    question: "A pizza surgiu na França?",      
+      answers: [
+        { option: "não", correct: true },
+        { option: "sim", correct: false },
+      
+      ],
+  },
 
 
-    {
-      question: "Os pandas são animais carnívoros?",      
-        answers: [
-          { option: "não", correct: true },
-          { option: "sim", correct: false },
-        
-        ],
-    },
+  {
+    question: "Os pandas são animais carnívoros?",      
+      answers: [
+        { option: "não", correct: true },
+        { option: "sim", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quantos oceanos existem na Terra?",      
-        answers: [
-          { option: "6", correct: false },
-          { option: "5", correct: true },
-        
-        ],
-    },
+  {
+    question: "Quantos oceanos existem na Terra?",      
+      answers: [
+        { option: "6", correct: false },
+        { option: "5", correct: true },
+      
+      ],
+  },
 
 
 
 
 
-    {
-      question: "Quantos centavos existem em uma moeda de um real?",      
-        answers: [
-          { option: "10", correct: false },
-          { option: "100", correct: true },
-        
-        ],
-    },
+  {
+    question: "Quantos centavos existem em uma moeda de um real?",      
+      answers: [
+        { option: "10", correct: false },
+        { option: "100", correct: true },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Quantos zeros há em um milhão?",      
-        answers: [
-          { option: "6", correct: true },
-          { option: "5", correct: false },
-        
-        ],
-    },
+  {
+    question: "Quantos zeros há em um milhão?",      
+      answers: [
+        { option: "6", correct: true },
+        { option: "5", correct: false },
+      
+      ],
+  },
 
 
 
-    {
-      question: "Qual o maior país do mundo?",     
-        answers: [
-          { option: "Canadá", correct: false },
-          { option: "Rússia", correct: true },
-        
-        ],
-    },
+  {
+    question: "Qual o maior país do mundo?",     
+      answers: [
+        { option: "Canadá", correct: false },
+        { option: "Rússia", correct: true },
+      
+      ],
+  },
 
 
 
